645072f8cc89c967f94eb9baee4055fa7c9e6852
Anton Pegov <antonpegov@gmail.com>
2020-08-07 23:58:19 +0200

    Create UPDATE and DELETE endpoints to Topics

--- src/main/java/io/antonpegov/jwt/controller/TopicController.java
+++ src/main/java/io/antonpegov/jwt/controller/TopicController.java
@@ -3,11 +3,8 @@ package io.antonpegov.jwt.controller;
 import io.antonpegov.jwt.model.Topic;
 import io.antonpegov.jwt.service.TopicService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -24,5 +21,20 @@ public class TopicController {
   @RequestMapping("/topics/{id}")
   public Topic getTopic(@PathVariable String id) {
     return topicService.getTopic(id);
+  }
+
+  @RequestMapping(method = RequestMethod.POST, value = "/topics")
+  public void addTopic(@RequestBody Topic topic) {
+    topicService.addTopic(topic);
+  }
+
+  @RequestMapping(method = RequestMethod.PUT, value = "/topics/{id}")
+  public void updateTopic(@PathVariable String id, @RequestBody Topic topic) {
+    topicService.updateTopic(id, topic);
+  }
+
+  @RequestMapping(method = RequestMethod.DELETE, value = "/topics/{id}")
+  public void deleteTopic(@PathVariable String id) {
+    topicService.deleteTopic(id);
   }
 }
--- src/main/java/io/antonpegov/jwt/service/TopicService.java
+++ src/main/java/io/antonpegov/jwt/service/TopicService.java
@@ -3,23 +3,42 @@ package io.antonpegov.jwt.service;
 import io.antonpegov.jwt.model.Topic;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class TopicService {
 
-    private List<Topic> topics = Arrays.asList(
+    private final List<Topic> topics = new ArrayList<>(List.of(
       new Topic("spring", "Spring Framework", "Spring Framework Description"),
       new Topic("java", "Java Framework", "Core Java Description"),
       new Topic("javascript", "Spring Framework", "Spring Framework Description")
-    );
+    ));
+
+    private Topic getTopicById (String id) {
+      return topics.stream().filter(t -> t.getId().equals(id)).findFirst().orElse(null);
+    }
 
     public List<Topic> getTopics() {
-        return topics;
+      return topics;
     }
 
     public Topic getTopic(String id) {
-        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+      return getTopicById(id);
+    }
+
+    public void addTopic(Topic topic) {
+      if (getTopicById(topic.getId()) == null) {
+          topics.add(topic);
+      }
+    }
+
+    public void updateTopic(String id, Topic topic) {
+      topics.remove(getTopicById(id));
+      topics.add(topic);
+    }
+
+    public void deleteTopic(String id) {
+        topics.remove(getTopicById(id));
     }
 }
