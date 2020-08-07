4766ae7a286b3fda22d3e8369992e9963e4eecd8
Anton Pegov <antonpegov@gmail.com>
2020-08-07 15:39:01 +0200

    Topic Controller should return Topics

--- src/main/java/io/antonpegov/jwt/topic/TopicController.java
+++ src/main/java/io/antonpegov/jwt/topic/TopicController.java
@@ -3,11 +3,21 @@ package io.antonpegov.jwt.topic;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 public class TopicController {
 
   @RequestMapping("/topics")
-  public String getAllTopics() {
-    return "All Topics";
+  public List<Topic> getAllTopics() {
+
+    List<Topic> l = Arrays.asList(
+      new Topic("spring", "Spring Framework", "Spring Framework Description"),
+      new Topic("java", "Java Framework", "Core Java Description"),
+      new Topic("javascript", "Spring Framework", "Spring Framework Description")
+    );
+
+    return l;
   }
 }
