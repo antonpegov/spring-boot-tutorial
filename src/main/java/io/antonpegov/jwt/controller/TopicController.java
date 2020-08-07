8fa7c2752f4f6cf14f59c831fb5d6d6421395f03
Anton Pegov <antonpegov@gmail.com>
2020-08-07 21:06:23 +0200

    Add endpoint to request topic by it's ID

--- src/main/java/io/antonpegov/jwt/controller/TopicController.java
+++ src/main/java/io/antonpegov/jwt/controller/TopicController.java
@@ -3,6 +3,7 @@ package io.antonpegov.jwt.controller;
 import io.antonpegov.jwt.model.Topic;
 import io.antonpegov.jwt.service.TopicService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +18,11 @@ public class TopicController {
 
   @RequestMapping("/topics")
   public List<Topic> getAllTopics() {
-
     return topicService.getTopics();
+  }
+
+  @RequestMapping("/topics/{id}")
+  public Topic getTopic(@PathVariable String id) {
+    return topicService.getTopic(id);
   }
 }
